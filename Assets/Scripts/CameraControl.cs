89897d106737b7b15d4d977971e0c6899a8acab9
jaldridge <89814068+jaldridg@users.noreply.github.com>
2025-12-03 18:41:27 -0600

    Camera affected by cursor

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,7 +7,7 @@ public class CameraControl : MonoBehaviour
 {
     GameObject player;
     [SerializeField] float cameraSnapiness = 2.0f;
-    Vector3 cameraVelocity;
+    [SerializeField] float cursorLimiting = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +18,31 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Camera will get pulled towards player
         Vector3 cam2D = new Vector3(transform.localPosition.x, 0.0f, transform.localPosition.z);
         Vector3 offsetVector = player.transform.localPosition - cam2D;
         Vector3 offsetVector2D = new Vector3(offsetVector.x, 0.0f, offsetVector.z).normalized;
-        transform.localPosition += offsetVector2D * offsetVector.magnitude * cameraSnapiness * Time.deltaTime;
+        Vector3 playerForce = offsetVector2D * offsetVector.magnitude;
+    
+        // Camera will get pulled by cursor
+        offsetVector = player.transform.position - GetMousePos();
+        offsetVector2D = new Vector3(-offsetVector.z, 0.0f, offsetVector.x).normalized;
+        Vector3 cameraForce = offsetVector2D * offsetVector.magnitude / cursorLimiting;
+
+        // Combine forces
+        transform.localPosition += (playerForce + cameraForce) * cameraSnapiness * Time.deltaTime;
+    }
+
+    Vector3 GetMousePos()
+    {
+        Plane p = new Plane(Vector3.up, 0);
+        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float d;
+        Vector3 mousePos = player.transform.position;
+        if (p.Raycast(r, out d))
+        {
+            mousePos = r.GetPoint(d);
+        }
+        return mousePos;
     }
 }
