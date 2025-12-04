8c11ef2f4ebb2db90294d3f803c7f189a8b00414
jaldridge <89814068+jaldridg@users.noreply.github.com>
2025-12-03 18:10:39 -0600

    Fixed camera movement bug

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,7 @@ public class CameraControl : MonoBehaviour
 {
     GameObject player;
     [SerializeField] float cameraSnapiness = 2.0f;
+    Vector3 cameraVelocity;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +18,9 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 cam2D = new Vector3(transform.position.x, 0.0f, transform.position.z);
-        Vector3 offsetVector = player.transform.position - cam2D;
-        Vector3 offsetVector2D = new Vector3(offsetVector.z, 0.0f, -offsetVector.x).normalized;
+        Vector3 cam2D = new Vector3(transform.localPosition.x, 0.0f, transform.localPosition.z);
+        Vector3 offsetVector = player.transform.localPosition - cam2D;
+        Vector3 offsetVector2D = new Vector3(offsetVector.x, 0.0f, offsetVector.z).normalized;
         transform.localPosition += offsetVector2D * offsetVector.magnitude * cameraSnapiness * Time.deltaTime;
     }
 }
